18d67a64a861358f9b1da155cb12294a372b372e
ChoiBoHui <105770711+ChoiBoHui@users.noreply.github.com>
2022-05-26 17:10:57 +0900

    Add files via upload

--- document/js/position05.js
+++ document/js/position05.js
@@ -2,8 +2,4 @@ $(function () {
     $('.smile').on('click', function(){
         $(this).toggleClass('active')
     })
-
-    $('.top_banner button').on('click', function(){
-        $('.top_banner .container').toggleClass('active');
-    })
 })
