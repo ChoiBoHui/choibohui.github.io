f2d653367ec2b2a9c058f7e7e4f0adae2bd7def7
ChoiBoHui <105770711+ChoiBoHui@users.noreply.github.com>
2022-08-24 09:48:07 +0900

    Add files via upload

---
+++ [07_모네 미술관]/js/main.js
@@ -0,0 +1,64 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    // 모바일 메뉴
+    const menuContainer = document.querySelector(".menu-container");
+    const gnb = document.querySelector(".gnb");
+
+    menuContainer.addEventListener("click", () => {
+        menuContainer.classList.toggle("active");
+        gnb.classList.toggle("open");
+    });
+
+
+
+
+
+    // 속초 호텔 코드
+    window.addEventListener('scroll', () => {
+        let sct = window.scrollY;
+        console.log(sct);
+        sct > 0
+            ? document.querySelector('#Header').classList.add('on')
+            : document.querySelector('#Header').classList.remove('on')
+        sct > 500
+            ? document.querySelector('.to_top').classList.add('on')
+            : document.querySelector('.to_top').classList.remove('on');
+    });
+
+    new Swiper('.MainSlider', {
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    })
+
+    const SCE_ELE = document.querySelectorAll('.active-event');
+    window.addEventListener('scroll', () => {
+        let sct = window.scrollY;
+        SCE_ELE.forEach(el => {
+            sct > el.offsetTop - 300
+                ? el.classList.add('on')
+                : el.classList.remove('on')
+        })
+    });
+
+    document.querySelector('.to_top').addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    });
+
+    document.querySelector('.mopen').addEventListener('click', (e) => {
+        const TG = e.currentTarget;
+        TG.classList.toggle('on');
+        document.querySelector('#Header').classList.toggle('ox')
+    });
+
+    document.querySelector('#Header').addEventListener('scroll', (e) => {
+        console.log(e)
+    })
+
+})
