fd174f17f3c480114afac56c527b53bd377afd54
choibohui <123dfga@naver.com>
2022-10-12 09:14:51 +0900

    20221011

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import ReactFullpage from '@fullpage/react-fullpage';
 import data from './portfolioDATA';
-import './main.scss'
+import './reset.css';
+import './main.scss';
+
 
 const App = () => {
     const [num, setNum] = useState(1);
     const [con, setCon] = useState('cover');
     const [on, setOn] = useState(false);
     const ac = data.map(it => it.anchor);
-    const Cover = () => {
-        return <div className={`cover ${on ? 'on' : ''}`}>나는 커버임</div>
-    }
 
+    const Cover = () => {
+        return (
+            <div className={`cover ${on ? 'on' : ''}`}>
+                <ul>
+                    {
+                        data.map((it, idx) => {
+                            return (
+                                <li>
+                                    <a href={`#${it.anchor}`} onClick={
+                                        () => setOn(!on)
+                                    }>{it.title}</a>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
     return (
-        <div className='PF'>
+        <div className='FP'>
             <Cover />
-            <header>
-                2023 choi portfolio {num} {con}
-                <button onClick={() => setOn(!on)}>커버가 나오는 버튼</button>
+            <header className='header'>
+                2023 lee portfolio {num} {con}
             </header>
+            <button onClick={() => setOn(!on)} className={`cover_open ${on ? 'on' : ''}`}>
+                <span>커버 나오는 버튼</span>
+            </button>
             <nav className='gnb'>
                 <ul>
                     {
@@ -39,7 +59,7 @@ const App = () => {
                 anchors={['cover', ...ac, 'copyright']}
                 afterLoad={(origin, destination) => {
                     setNum(destination.index + 1);
-                    setCon(destination.ancor);
+                    setCon(destination.anchor)
                 }
                 }
 
@@ -47,31 +67,38 @@ const App = () => {
                     return (
                         <ReactFullpage.Wrapper>
                             <div className="section">
-                                <p>Section 1 (welcome to fullpage.js)</p>
-                                <a href="#pf01">01</a>
-                                <button onClick={() => fullpageApi.moveSectionDown()}>
-                                    Click me to move down
-                                </button>
+                                <div className='inner'>
+                                    <p>Section 1 (welcome to fullpage.js)</p>
+                                    <a href="#pf01">01</a>
+                                    <button onClick={() => fullpageApi.moveSectionDown()}>
+                                        Click me to move down
+                                    </button>
+                                </div>
                             </div>
                             {
                                 data.map(it => {
                                     return (
                                         <div className="section">
-                                            <p>{it.id}</p>
+                                            <div className='inner'>
+                                                <p>{it.id}</p>
+                                                {
+                                                    it.color?.map(color => <li style={{ background: color }}>{color}</li>)
+                                                }
+                                            </div>
                                         </div>
                                     )
                                 })
                             }
-
                             <div className="section">
-                                <p>Section 3</p>
+                                <p className='inner'>Section 2</p>
                             </div>
                         </ReactFullpage.Wrapper>
                     );
                 }}
             />
         </div>
+
     )
 }
 
-export default App;
+export default App
